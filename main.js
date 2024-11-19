a7df9f1d523179e56b37886cf355e84408bc4227
Valentin Björkli Castillo <valentin.bjorkli@outlook.com>
2024-11-19 11:37:37 +0100

    add products-list array

---
+++ main.js
@@ -0,0 +1,129 @@
+
+// ---------------------------------------------
+// ------------ Products section ---------------
+// ---------------------------------------------
+
+//Array of products (donuts)
+const donut_products = [ //TODO: Lägg till bilder
+	{
+		id: 'klassisk_sockermunk',
+		name: 'Klassisk Sockermunk',
+		price: 10,
+		rating: 3.5,
+		category: 'Klassisk',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+	{
+		id: 'coklad_strossel',
+		name: 'Choklad med regnbågsströssel',
+		price: 13,
+		rating: 4,
+		category: 'Choklad drömmar', //ändra
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_cat_donut.jpg',
+			alt: 'donut in a cartoon artstyle where the glacing is an a form of a pink cat',
+		},
+	},
+	{
+		id: 'salt_karamell_havssalt',
+		name: 'Salt Karamell och Havssalt',
+		price: 17,
+		rating: 5,
+		category: 'Friterad lyx',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/frog_donut.jpg',
+			alt: 'donut where the glacing looks like a smiling and happy frog',
+		},
+	},
+    {
+		id: 'citron_vallmo',
+		name: 'Citron och Vallmofrö',
+		price: 12,
+		rating: 3,
+		category: 'Syrligt!',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'hallon_pistage',
+		name: 'Hallon och Pistage',
+		price: 15,
+		rating: 4,
+		category: 'Bär på en munk',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'nutella_hasselnot',
+		name: 'Nutella och Hasselnöt',
+		price: 20,
+		rating: 5,
+		category: 'Choklad drömmar',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'vanilj_blabar',
+		name: 'Vanilj och Blåbär',
+		price: 13,
+		rating: 4,
+		category: 'Bär på en munk',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'matcha_kokos',
+		name: 'Matcha och Kokos',
+		price: 18,
+		rating: 3.5,
+		category: 'Kryddiga och värmande',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'apple_kanel',
+		name: 'Äpple och Kanel',
+		price: 18,
+		rating: 4.5,
+		category: 'Kryddiga och värmande',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+    {
+		id: 'jordgubbscheesecake',
+		name: 'Jordgubscheesecake',
+		price: 20,
+		rating: 5,
+		category: 'Bär på en munk',
+		amount: 0,
+		img: {
+			url: 'assets/product_images/cartoon_pink_donut.jpg',
+			alt: 'Pink donut in a cartoon artstyle',
+		},
+	},
+];
+
